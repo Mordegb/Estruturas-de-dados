51f65c01e670c4bfb0448550e2caa11040a1c4f5
Cadu <cadumalmeida13@gmail.com>
2025-09-19 15:25:56 -0300

    array com arquivo

--- ArrayMemory.c
+++ ArrayMemory.c
@@ -2,7 +2,6 @@
 #include <stdbool.h>
 #define MAX 100
 #define ARQUIVO "dados_array.txt"
-
 int qtd = 0;
 int vetor[MAX];
 
@@ -12,36 +11,37 @@ int tamanho() {
 }
 
 void salvarDados() {
+    //salva os dados para o arquivo de texto
     FILE *file = fopen(ARQUIVO, "w");
     if (file == NULL) {
-        printf("Erro ao criar arquivo!\n");
+        printf("erro pra criar arquivo\n");
         return;
     }
 
-    // Salva a quantidade primeiro
+    // escreve a quantidade de valores no arquivo
     fprintf(file, "%d\n", qtd);
 
-    // Salva todos os elementos
+    // escrever cada valor no arquivo
     for (int i = 0; i < qtd; i++) {
         fprintf(file, "%d\n", vetor[i]);
     }
 
     fclose(file);
-    printf("Dados salvos com sucesso!\n");
+    printf("deu bom salvar os dados\n");
 }
 
-// Função para carregar os dados do arquivo
+// função para carregar os dados do arquivo
 void carregarDados() {
     FILE *file = fopen(ARQUIVO, "r");
     if (file == NULL) {
-        printf("Arquivo não encontrado. Iniciando com array vazio.\n");
+        printf("arquivo não encontrado, iniciando um array vazio.(deu ruim) \n");
         return;
     }
 
-    // Lê a quantidade
+    // Le a quantidade de valores no arquivo
     fscanf(file, "%d", &qtd);
 
-    // Lê todos os elementos
+    // Lê os os elementos
     for (int i = 0; i < qtd; i++) {
         fscanf(file, "%d", &vetor[i]);
     }
@@ -52,6 +52,7 @@ void carregarDados() {
 
 //insere um valor numa posição especifica do array
 bool inserirPosicao(int posi,int valor) {
+
     if (qtd == MAX || posi < 0 || posi > qtd) {
         return false;
     }
@@ -64,7 +65,7 @@ bool inserirPosicao(int posi,int valor) {
     return true;
 }
 
-bool inserirFinal(int valor) {
+bool inserirFinal(int valor) { //insere um valor no final do array
     if (qtd == MAX) {
         return false; //vetor ta cheio
     }
@@ -114,108 +115,95 @@ void imprimirVetor() {
 int main(){
     // Carrega os dados ao iniciar o programa
     carregarDados();
-    imprimirVetor();
 
     int opt;
-    do {
-        printf("\nDigite um numero de acordo com a opção:\n");
-        printf("1 - inserir em posição.  2 - inserir no final.\n");
-        printf("3 - excluir posição.     4 - remover valor.\n");
-        printf("5 - buscar valor.        6 - imprimir array.\n");
-        printf("7 - salvar dados.        0 - sair\n"); // Nova opção
-        printf("Opção: ");
-        scanf("%d", &opt);
 
-        switch (opt) {
-            case 1: {
-                int valor, pos;
-                printf("Digite um valor e posição para inserir no array: ");
-                scanf("%d %d", &valor, &pos);
-                if (inserirPosicao(pos, valor)) {
-                    printf("Valor %d inserido na posição %d\n", valor, pos);
-                } else {
-                    printf("Erro ao inserir!\n");
-                }
-                break;
+    printf("\nDigite um numero de acordo com a opção:\n");
+    printf("1 - inserir em posição.  2 - inserir no final.\n");
+    printf("3 - excluir posição.     4 - remover valor.\n");
+    printf("5 - buscar valor.        6 - imprimir array.\n");
+    printf("Opção: ");
+    scanf("%d", &opt);
+
+    switch (opt) {
+        case 1: {
+            int valor, pos;
+            printf("Digite um valor para inserir no array: ");
+            scanf("%d", &valor);
+            printf("colocar o valor %d em qual posição do array: " , valor);
+            scanf("%d", &pos);
+            if (inserirPosicao(pos, valor)) {
+                printf("Valor %d inserido na posição %d\n", valor, pos);
+            }
+            else {
+                printf("Erro ao inserir!\n");
+            }
+            salvarDados();
+            break;
             }
 
-            case 2: {
-                int valor;
-                printf("Digite um valor para inserir no final do array: ");
-                scanf("%d", &valor);
-                if (inserirFinal(valor)) {
-                    printf("Valor %d inserido no final\n", valor);
-                } else {
-                    printf("Erro: vetor cheio!\n");
-                }
-                break;
+        case 2: {
+            int valor;
+            printf("Digite um valor para inserir no final do array: ");
+            scanf("%d", &valor);
+            if (inserirFinal(valor)) {
+                printf("Valor %d inserido no final\n", valor);
+            }
+            else {
+                printf("Erro: vetor cheio!\n");
+            }
+            salvarDados();
+            break;
             }
 
-            case 3: {
-                int posi_excluir;
-                printf("Digite uma posição do array para excluir: ");
-                scanf("%d", &posi_excluir);
-                if (removerPosicao(posi_excluir)) {
-                    printf("Posição %d removida\n", posi_excluir);
-                } else {
-                    printf("Erro: posição inválida!\n");
-                }
-                break;
+        case 3: {
+            int posi_excluir;
+            printf("Digite uma posição do array para excluir: ");
+            scanf("%d", &posi_excluir);
+            if (removerPosicao(posi_excluir)) {
+                printf("Posição %d removida\n", posi_excluir);
+            }
+            else {
+                printf("Erro: posição inválida!\n");
+            }
+            salvarDados();
+            break;
             }
 
-            case 4: {
-                int valor;
-                printf("Digite um valor para remover do array: ");
-                scanf("%d", &valor);
-                if (removerValor(valor)) {
-                    printf("Valor %d removido\n", valor);
-                } else {
-                    printf("Valor não encontrado!\n");
-                }
-                break;
+        case 4: {
+            int valor;
+            printf("Digite um valor para remover do array: ");
+            scanf("%d", &valor);
+            if (removerValor(valor)) {
+                printf("Valor %d removido\n", valor);
+            }
+            else {
+                printf("Valor não encontrado!\n");
+            }
+            salvarDados();
+            break;
             }
 
-            case 5: {
-                int valor;
-                printf("Digite um valor para buscar no array: ");
-                scanf("%d", &valor);
-                int pos = buscarValor(valor);
-                if (pos != -1) {
-                    printf("Valor %d encontrado na posição %d\n", valor, pos);
-                } else {
-                    printf("Valor %d não encontrado\n", valor);
-                }
-                break;
+        case 5: {
+            int valor;
+            printf("Digite um valor para buscar no array: ");
+            scanf("%d", &valor);
+            int pos = buscarValor(valor);
+            if (pos != -1) {
+                printf("Valor %d encontrado na posição %d\n", valor, pos);
+            }
+            else {
+                printf("Valor %d não encontrado\n", valor);
+            }
+            salvarDados();
+            break;
             }
 
-            case 6: {
-                imprimirVetor();
-                break;
-            }
-
-            case 7: {
-                salvarDados();
-                break;
-            }
-
-            case 0: {
-                // Pergunta se quer salvar antes de sair
-                char resposta;
-                printf("Deseja salvar os dados antes de sair? (s/n): ");
-                scanf(" %c", &resposta); // Note o espaço antes do %c para consumir o enter anterior
-
-                if (resposta == 's' || resposta == 'S') {
-                    salvarDados();
-                }
-                printf("Saindo...\n");
-                break;
-            }
-
-            default: {
-                printf("Opção inválida!\n");
-            }
+        case 6: {
+            imprimirVetor();
+            break;
         }
-    } while (opt != 0);
+    }
 
     return 0;
 }
